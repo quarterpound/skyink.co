b84078d107eb099cf196b3b3c4683751ef83ea9e
aliteqqed <127309376+aliteqqed@users.noreply.github.com>
2024-02-13 00:44:29 +0400

    Added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="py-6">
-      <div className="container max-w-[800px] grid gap-5">
-        <h1 className="text-5xl text-center font-semibold mx-auto">
-          From Sun-Kissed Threads to Soulful Sculpts: Every Treasure Blooms in
-          Your Haven
+    <main className="py-20">
+      <section className="mx-auto max-w-[500px]">
+        <h1 className="text-center text-4xl">
+          Your <span className="italic">dream</span> home awaits - make it a
+          reality with <span className="italic">Skyink</span>
         </h1>
-        <button className="px-4 py-2 border-black w-fit mx-auto h-[40px] border rounded-full">
-          Explore Now
-        </button>
-      </div>
+      </section>
+      <section className="grid grid-cols-3 gap-5"></section>
     </main>
   );
 }
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,22 +1,34 @@
-import Link from "next/link";
-import LogoIcon from "../icons/logo";
-import {
-  Bars3Icon,
-  MagnifyingGlassIcon,
-  ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
+import Link, { LinkProps } from "next/link";
+import LogoGradient from "@/components/icons/logo_gradient";
+import { AnchorHTMLAttributes } from "react";
+import { cn } from "@/utils/tailwind";
+
+const NavLink = ({
+  className,
+  ...props
+}: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>) => (
+  <Link {...props} className={cn("uppercase", className)} />
+);
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-10 md:px-10 px-6 mx-auto">
-      <Bars3Icon className="w-6 h-6" />
-      <LogoIcon className="w-[150px]" />
-      <div className="flex gap-4">
-        <MagnifyingGlassIcon className="w-6 h-6" />
-        <Link href={"/cart"}>
-          <ShoppingCartIcon className="w-6 h-6" />
-        </Link>
-      </div>
+    <header className="container flex h-32 w-full items-center justify-between py-10">
+      <nav className="flex gap-5">
+        <NavLink href={"/shop"} className="uppercase">
+          Shop
+        </NavLink>
+        <NavLink href={"/about"} className="uppercase">
+          About
+        </NavLink>
+      </nav>
+      <Link href={"/"}>
+        <LogoGradient className="h-24 w-fit" />
+      </Link>
+      <nav>
+        <NavLink href={"/cart"} className="uppercase">
+          Cart
+        </NavLink>
+      </nav>
     </header>
   );
 };
